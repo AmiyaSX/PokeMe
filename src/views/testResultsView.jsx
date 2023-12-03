af37eabf2fde8bf5cd2b98a89adefb4e12c31188
Jeffrey Chang <zw.jchang@gmail.com>
2023-12-03 21:58:52 +0100

    testResultsView buttons back

--- src/views/testResultsView.jsx
+++ src/views/testResultsView.jsx
@@ -3,6 +3,9 @@ import { getPokemonDetails, getPokemonSpecies } from "../pokemonService";
 import Banner from "./components/banner";
 import BackButton from "./components/backbutton";
 import PokeItem from "./components/pokeItem";
+import Icon1 from "../assets/images/icon_1.png";
+import Icon2 from "../assets/images/icon_2.png";
+import Icon3 from "../assets/images/insignia.png";
 import "/src/style.css";
 
 function TestResultsView(props) {
@@ -11,10 +14,10 @@ function TestResultsView(props) {
 
     useEffect(() => {
         const fetchPokemonData = async () => {
-            const pokemonData = await getPokemonDetails('pikachu'); //change so it displays the pokemon from test
+            const pokemonData = await getPokemonDetails('pikachu'); // Change to dynamic Pokemon based on test
             setPokemon(pokemonData);
 
-            const speciesData = await getPokemonSpecies('pikachu'); //change so it displays the pokemon from tests
+            const speciesData = await getPokemonSpecies('pikachu'); // Change to dynamic species based on test
             setSpecies(speciesData);
         };
 
@@ -22,6 +25,17 @@ function TestResultsView(props) {
     }, []);
 
     const pokemonImageURL = pokemon ? `https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg` : '';
+
+    // Someone later will implement the onClicks here
+    const handleTryAgain = () => {
+        console.log("Try Again clicked");
+    };
+    const handleSaveResult = () => {
+        console.log("Save My Result clicked");
+    };
+    const handleShare = () => {
+        console.log("Share clicked");
+    };
 
     return (
         <div>
@@ -44,10 +58,24 @@ function TestResultsView(props) {
                                 <p>Abilities: {pokemon.abilities.map((ability) => ability.ability.name).join(', ')}</p>
                                 <p>Fact: {species.flavor_text_entries.find(entry => entry.language.name === 'en').flavor_text}</p>
                             </div>
-                        ) : (
-                            "Loading Pokémon details..."
-                        )}
+                            ) : (
+                                "Loading Pokémon details..."
+                            )}
                     </div>
+                </div>
+                <div className="flextRowParent">
+                    <button className="button_2" onClick={handleTryAgain}>
+                        <img src={Icon1} alt="Try Again" width={58} height={58}/>
+                        <div>Try Again!</div>
+                    </button>
+                    <button className="button_2" onClick={handleSaveResult}>
+                        <img src={Icon2} alt="Save My Result" width={58} height={58}/>
+                        <div>Save My Result!</div>
+                    </button>
+                    <button className="button_2" onClick={handleShare}>
+                        <img src={Icon3} alt="Share" width={58} height={58}/>
+                        <div>Share!</div>
+                    </button>
                 </div>
             </div>
         </div>
