1958f634885ee244e861b669be7a8c1955155c17
Yilei <yilei@kth.se>
2023-12-05 18:00:48 +0100

    chore(pokeme): configure github deployment

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,36 @@
 import fs from "fs";
 import path from "path";
 
-import { defineConfig } from 'vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
-import solidPlugin from 'vite-plugin-solid';
-import virtualHtml from 'vite-plugin-virtual-html'
+import solidPlugin from "vite-plugin-solid";
+import virtualHtml from "vite-plugin-virtual-html";
+import ViteGHPages from "vite-plugin-gh-pages";
 // import reactJsxPlugin from '@vitejs/plugin-react';
 // if solved-X.js (or .jsx or .vue or .css) exists, then solved-X.js is tested, otherwise  X.js
-const prefix=  fs.existsSync("./src/solved-utilities.js")?"solved-":"";
+const prefix = fs.existsSync("./src/solved-utilities.js") ? "solved-" : "";
 
 // we map every tw/tw*.jsx file to a virtual HTML available at http://localhost:PORT/tw*.html
 
-const virtualModuleId = 'virtual:my-module'
-  const resolvedVirtualModuleId = '\0' + virtualModuleId
-const pages= {
-    // map /react.html
-    "react":{
-        entry: '/src/reactjs/'+prefix+'index.jsx',
-        title:"PokeMe", 
-        body: '<div id="root"></div>'
-    },
+const virtualModuleId = "virtual:my-module";
+const resolvedVirtualModuleId = "\0" + virtualModuleId;
+const pages = {
+  // map /react.html
+  react: {
+    entry: "/src/reactjs/" + prefix + "index.jsx",
+    title: "PokeMe",
+    body: '<div id="root"></div>',
+  },
 };
 
-pages.index= pages.test;
+pages.index = pages.test;
 
 export default defineConfig({
-    plugins: [
-        //solidPlugin(),
-        // reactJsxPlugin(),
-/*        {
+  plugins: [
+    //solidPlugin(),
+    // reactJsxPlugin(),
+    /*        {
             name: 'my-plugin', // required, will show up in warnings and errors
             resolveId(id) {
                 console.log(id);
@@ -44,19 +45,20 @@ export default defineConfig({
                 }
             },
         },*/
-        nodePolyfills({ protocolImports: true,}),   // needed by mocha
-        virtualHtml({pages})      // HTML mappings
-    ],
-    server: {
-        port: 8080,
-    },
-    define: {
-        TEST_PREFIX: JSON.stringify(prefix),
-        __VUE_OPTIONS_API__:JSON.stringify(true),
-        __VUE_PROD_DEVTOOLS__:JSON.stringify(true),
-    },
-    build: {
-        target: 'esnext',   // javascript version to target: latest
-        chunkSizeWarningLimit: 600,
-    },
+    nodePolyfills({ protocolImports: true }), // needed by mocha
+    virtualHtml({ pages }), // HTML mappings
+    ViteGHPages(),
+  ],
+  server: {
+    port: 8080,
+  },
+  define: {
+    TEST_PREFIX: JSON.stringify(prefix),
+    __VUE_OPTIONS_API__: JSON.stringify(true),
+    __VUE_PROD_DEVTOOLS__: JSON.stringify(true),
+  },
+  build: {
+    target: "esnext", // javascript version to target: latest
+    chunkSizeWarningLimit: 600,
+  },
 });
