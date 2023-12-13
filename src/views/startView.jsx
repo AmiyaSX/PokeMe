2a57543e5cc1e81c200aada13c55dd74e1116d78
AmiyaSX <2668590277@qq.com>
2023-12-13 07:22:55 +0100

    add start arrow animation in startview

--- src/views/startView.jsx
+++ src/views/startView.jsx
@@ -7,10 +7,15 @@ function StartView(props) {
     <div className="slash">
       <h1>POKÉME</h1>
       <div className="text">Take our engaging personality test, discover your perfect Pokémon match with PokéMe...</div>
-      <div className="container">
-        <div className="login-text" onClick={goToLogin}>Journey Begins!</div>
+      <div className="start-container">
+          <div className="login-text" onClick={goToLogin}>Journey Begins!</div>
+          <div className="arrows-body" onClick={goToLogin}>
+              <div className="arrow"></div>
+              <div className="arrow"></div>
+              <div className="arrow"></div>
+          </div>
+        </div>
       </div>
-    </div>
   );
 }
 
