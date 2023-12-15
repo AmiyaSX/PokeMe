8f812a9b0d497318ef6a936580ba09b85063ef2b
Jeffrey Chang <zw.jchang@gmail.com>
2023-12-15 23:36:33 +0100

    mobile login page

--- src/views/loginView.jsx
+++ src/views/loginView.jsx
@@ -5,42 +5,51 @@ import "/src/style.css";
 
 function LoginView(props) {
   return (
-    <div className="">
+    <div className="login-view-container">
       <Banner text="Login to Test!" />
 
-      <div>
+      <div className="input-group">
+        <label htmlFor="email" className="input-label">Email:</label>
         <InputForm
-          text="Email: "
+          id="email"
           type="text"
           onChange={(e) => props.setEmail(e.target.value)}
         />
+      </div>
+
+      <div className="input-group">
+        <label htmlFor="password" className="input-label">Password:</label>
         <InputForm
-          text="Password: "
+          id="password"
           type="password"
           onChange={(e) => props.setPassword(e.target.value)}
         />
-        {props.error && (
-          <div className="error-message">
-            <img
-              className="error-message-icon"
-              src="https://cdn-icons-png.flaticon.com/128/9647/9647409.png"
-              alt="Error"
-            />
-            {props.error}
-          </div>
-        )}
-        <div className="login-pikachu">
-          <img src={PikachuHello} alt="pikachu hello" />
+      </div>
+
+      {props.error && (
+        <div className="error-message">
+          <img
+            className="error-message-icon"
+            src="https://cdn-icons-png.flaticon.com/128/9647/9647409.png"
+            alt="Error"
+          />
+          {props.error}
         </div>
-        <div className="flextRowParent">
-          <button className="button_1" onClick={props.handleLogin}>
-            Login
-          </button>
-        </div>
-        <div className="columnContainer">
-          <div className="normalText">
-            Don't have an account yet? <a href="#/register">Register Here!</a>
-          </div>
+      )}
+
+      <div className="login-pikachu">
+        <img src={PikachuHello} alt="pikachu hello" />
+      </div>
+
+      <div className="flextRowParent">
+        <button className="button_1" onClick={props.handleLogin}>
+          Login
+        </button>
+      </div>
+
+      <div className="columnContainer">
+        <div className="normalText">
+          Don't have an account yet? <a href="#/register">Register Here!</a>
         </div>
       </div>
     </div>
